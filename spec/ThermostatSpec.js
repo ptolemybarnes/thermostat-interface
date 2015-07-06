0a50c2394ed1b60711f007408156a19d8d560b3e
Nicola Hughes <nicolarhughes@gmail.com>
2015-07-06 17:27:21 +0100

    Allowing temperature to be reset

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -18,6 +18,37 @@ describe('Thermostat', function() {
 
     it('can be decreased by 1', function() {
       thermostat.down();
+      expect(thermostat.show()).toEqual(19);
+    });
+
+  });
+
+  describe('has limited range', function() {
+
+    it('cannot go below 10', function() {
+      for (count = 20; count > 0; count--){
+        thermostat.down();
+      };
+      expect(thermostat.show()).toEqual(10);
+    });
+
+    it('cannot go above 25 if the power save mode is on', function() {
+      for (count = 0; count < 100; count++){
+        thermostat.up();
+      };
+      expect(thermostat.show()).toEqual(25);
+    });
+
+    it('cannot go above 32 if the power save mode is off', function() {
+      thermostat.changeMode();
+      for (count = 0; count < 100; count++){
+        thermostat.up();
+      };
+      expect(thermostat.show()).toEqual(32);
+    });
+
+    it('can be reset to 20', function() {
+      thermostat.reset();
       expect(thermostat.show()).toEqual(20);
     });
 
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,15 +1,27 @@
-var Thermostat = function() {};
-
-var temperature = 20
+var Thermostat = function() {
+  this.temperature = 20
+  this.powerSaveMode = true
+};
 
 Thermostat.prototype.show = function () {
-  return temperature
+  return this.temperature
 };
 
 Thermostat.prototype.up = function () {
-  temperature += 1
+  if (this.powerSaveMode && this.temperature < 25) { this.temperature += 1 };
+  if (!this.powerSaveMode && this.temperature < 32) { this.temperature += 1 };
 };
 
 Thermostat.prototype.down = function () {
-  temperature -= 1
+  if (this.temperature > 10) {
+    this.temperature -= 1
+  }
+};
+
+Thermostat.prototype.changeMode = function () {
+  this.powerSaveMode = !this.powerSaveMode
+};
+
+Thermostat.prototype.reset = function() {
+  this.temperature = 20
 };
