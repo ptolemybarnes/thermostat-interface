4a89cdaf86f5a8f8ed068e0848b0714ffae9c087
Natalie Akam <natalie.akam@gmail.com>
2015-07-06 15:52:43 +0100

    Thermostat starts at 20*C

---
+++ spec/ThermostatSpec.js
@@ -0,0 +1,14 @@
+describe('Thermostat', function() {
+
+  var thermostat;
+
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
+
+  it('starts at 20 degrees ', function() {
+    expect(thermostat.currentTemp()).toEqual(20);
+  });
+
+
+});
---
+++ src/Thermostat.js
@@ -0,0 +1,5 @@
+var Thermostat = function() {};
+
+Thermostat.prototype.currentTemp = function() {
+  return 20;
+};
