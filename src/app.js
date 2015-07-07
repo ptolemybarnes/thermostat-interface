d0c3204559dd87f0e4208725de6fbb6187ef1ba7
Nicola Hughes <nicolarhughes@gmail.com>
2015-07-07 12:28:05 +0100

    Changing colour of temperature

--- src/app.js
+++ src/app.js
@@ -7,11 +7,13 @@ function showTemp() {
 
 function upTemp() {
   thermostat.up();
+  changeColour();
   showTemp();
 }
 
 function downTemp() {
   thermostat.down();
+  changeColour();
   showTemp();
 }
 
@@ -21,11 +23,13 @@ function powerSaveMode() {
 
 function resetTemp() {
   thermostat.reset();
+  changeColour();
   showTemp();
 }
 
 function changeColour() {
   var temperature = document.getElementById('temperature');
-  if(thermostat.setting() === "low") {}
-  if(thermostat.setting() === "high") {}
+  if(thermostat.setting() === "low") { temperature.setAttribute('style', 'color: green;') }
+  if(thermostat.setting() === "medium") { temperature.setAttribute('style', 'color: orange;') }
+  if(thermostat.setting() === "high") { temperature.setAttribute('style', 'color: red;') }
 }
