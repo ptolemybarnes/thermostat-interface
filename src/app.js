9ab0aa1e8b77188a28afa860d608941a03e547dc
Nicola Hughes <nicolarhughes@gmail.com>
2015-07-08 10:49:38 +0100

    Refactoring down button function to use JQuery

--- src/app.js
+++ src/app.js
@@ -11,21 +11,15 @@ function showTemp() {
   return changeColour();
 }
 
-$('#up').click('click', function upTemp() {
+$('#up').click(function upTemp() {
   thermostat.up();
   showTemp();
 });
 
-
-// document.getElementById('up').onclick = function upTemp() {
-//   thermostat.up();
-//   showTemp();
-// };
-
-document.getElementById('down').onclick = function downTemp() {
+$('#down').click(function downTemp() {
   thermostat.down();
   showTemp();
-};
+});
 
 document.getElementById('powerSave').onchange = function powerSaveMode() {
   thermostat.changeMode();
