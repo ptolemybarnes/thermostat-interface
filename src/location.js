2f5fadf33cba90eb71b13a175cf19a98ed5022a1
Nicola Hughes <nicolarhughes@gmail.com>
2015-07-08 18:26:45 +0100

    Modifying call to get temperature to use the browser location

--- src/location.js
+++ src/location.js
@@ -8,11 +8,19 @@ function GetLocation(location) {
     getAddress(latitude, longitude);
 }
 
-
 function getAddress(latitude, longitude) {
   var geocoder = new google.maps.Geocoder();
   geocoder.geocode({'latLng': new google.maps.LatLng(latitude, longitude) }, function(results, status) {
-    $('span#location').html(results[0].formatted_address);
-    console.log(results[0].formatted_address)
+    $('span#location').html('The current temperature at ' + results[0].address_components[1].long_name);
+    var location = results[0].address_components[3].long_name
+    console.log(results[0].address_components[3].long_name)
+    getTemperature(location);
   });
 }
+
+function getTemperature(location){
+  var request = 'http://api.openweathermap.org/data/2.5/weather?q=' + location;
+  $.getJSON(request, function(data) {
+    $('#weather').html(' is : ' + Math.round(data.main.temp - 273.15).toString() + '&#8451;');
+    });
+};
